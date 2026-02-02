bd05e3c93398dccbce77a41dd83a3279fe6dd226
AcrylicMadness <akirill1942@me.com>
2026-02-02 19:35:18 +0300

    Clean up setup.sh by removing echo statements
    Remove unnecessary echo commands from setup script

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ git clone https://github.com/AcrylicMadness/SwiftGodot-Integrator
 cd SwiftGodot-Integrator
 swift build --configuration release
 BIN_PATH="$(swift build --configuration release --show-bin-path)"
-echo ${PWD}
-echo $RD
 cp $BIN_PATH/sgint $RD/sgint
 cd $RD
 rm -rf "SwiftGodot-Integrator"
